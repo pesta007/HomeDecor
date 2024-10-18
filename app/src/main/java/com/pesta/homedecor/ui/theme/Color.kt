0987fe1b16d8dcb9619d5cb2dd3be8c0af3a1f21
pesta <kceknm@gamil.com>
2024-10-18 22:42:52 +0300

    implemented the design theme(colors and typography)

--- app/src/main/java/com/pesta/homedecor/ui/theme/Color.kt
+++ app/src/main/java/com/pesta/homedecor/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.pesta.homedecor.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF363130)
+val Salmon = Color(0xFFF4B5A4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Terracotta = Color(0xFFCC7861)
+val BrownRosy = Color(0xFFDCBEB6)
+val Beige = Color(0xFFFAF0E6)
+val OrangeDark = Color(0xFFCC7861)
--- app/src/main/java/com/pesta/homedecor/ui/theme/Type.kt
+++ app/src/main/java/com/pesta/homedecor/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.pesta.homedecor.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pesta.homedecor.R
 
 // Set of Material typography styles to start with
+
+val LeagueSpartan = FontFamily(
+    Font(R.font.league_spartan, FontWeight.Normal),
+    Font(R.font.league_spartan_light, FontWeight.Light)
+)
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = Poppins,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = LeagueSpartan,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal
     )
-    */
 )
