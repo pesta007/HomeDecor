db7cec0efe3a735cad686099b6edbec080a0000c
pesta <kceknm@gamil.com>
2024-10-23 15:20:53 +0300

    add PasswordInputField to handle password insertion.

--- features/login/src/main/java/com/pesta/login/presentation/login/components/InputField.kt
+++ features/login/src/main/java/com/pesta/login/presentation/login/components/InputField.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -24,7 +25,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.PlatformImeOptions
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -33,7 +37,49 @@ fun InputField(
     modifier: Modifier = Modifier,
     placeholder: String = "",
     inputFieldColors: InputFieldColors = InputFieldDefaults.inputFieldColors(),
-    textStyle: TextStyle = InputFieldDefaults.textStyle()
+    textStyle: TextStyle = InputFieldDefaults.textStyle(),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(autoCorrect = false),
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+) {
+    BasicInputField(
+        modifier = modifier,
+        placeholder = placeholder,
+        inputFieldColors = inputFieldColors,
+        textStyle = textStyle,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
+    )
+}
+
+@Composable
+fun PasswordInputField(
+    modifier: Modifier = Modifier,
+    placeholder: String = "",
+    inputFieldColors: InputFieldColors = InputFieldDefaults.inputFieldColors(),
+    textStyle: TextStyle = InputFieldDefaults.textStyle(),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(autoCorrect = false),
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+) {
+    BasicInputField(
+        modifier = modifier,
+        placeholder = placeholder,
+        inputFieldColors = inputFieldColors,
+        textStyle = textStyle,
+        keyboardOptions = keyboardOptions.copy(keyboardType = KeyboardType.Password),
+        keyboardActions = keyboardActions,
+        visualTransformation = PasswordVisualTransformation()
+    )
+}
+
+@Composable
+fun BasicInputField(
+    modifier: Modifier = Modifier,
+    placeholder: String = "",
+    inputFieldColors: InputFieldColors = InputFieldDefaults.inputFieldColors(),
+    textStyle: TextStyle = InputFieldDefaults.textStyle(),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(autoCorrect = false),
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None
 ) {
     var text by rememberSaveable {
         mutableStateOf("")
@@ -48,7 +94,9 @@ fun InputField(
         textStyle = textStyle.copy(color = inputFieldColors.contentColor),
         singleLine = true,
         cursorBrush = SolidColor(inputFieldColors.contentColor),
-        keyboardOptions = KeyboardOptions(autoCorrect = false, keyboardType = KeyboardType.Email),
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        visualTransformation = visualTransformation,
         decorationBox = {innerTextField ->  
             Box(modifier = Modifier.padding(start = 20.dp, end = 16.dp, top = 14.dp, bottom = 14.dp)){
                 Text(
